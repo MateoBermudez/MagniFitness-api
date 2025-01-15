98ab9fe05e1662e03d633b2f61fecb2c57b49a96
MateoBermudez <mateo.bermudez2910@gmail.com>
2025-01-14 19:03:43 -0500

    Add product-microservice initial structure
    Product-Microservice was created using spring template

---
+++ product-microservice/src/test/java/com/devcrew/productmicroservice/ProductMicroserviceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.devcrew.productmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class ProductMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
